6c403e5408ae169f5f4cccc4aab189e73f00a756
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2021-03-09 10:30:46 +0300

    feat: Сделать отоброжение списка задач и логику изменения классов при выполнении

--- src/Components/FullCard/Task/index.jsx
+++ src/Components/FullCard/Task/index.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './styles.css';
 
 export default function TaskCard(props) {
   return (
     <div className="task_card">
-      <ul>
-        {props.card.task?.map((el) => {
-          return <li key={el.uid}>{el.value}
-          {el.status === 'work' ? <button>Comleted</button> : ''}
-          </li>;
+      <ul className="task_card_list">
+        {props.card.task?.map((el, i) => {
+          return (
+            <li
+              className={`task_card__item ${
+                el.status ? 'task_card__item_finish' : 'task_card__item_inWork'
+              }`}
+              key={el.uid}
+            >
+              {i + 1}. {el.value}
+              {props.card.status === 'work' ? (
+                <button className="task_card__comleted_btn">Comleted</button>
+              ) : (
+                ''
+              )}
+            </li>
+          );
         })}
       </ul>
     </div>
