5b91bb6b85029a6cf5d6aeab2fe218d6ed27799b
Ivan Sosnovich <ivan.sosnovich1987@gmail.com>
2021-03-12 10:22:45 +0300

    fea: Добавить иконку что на карточке появился инвайт

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 import NoMoneyIcon from '../../image/card/no_many.png';
 import MoneyIcon from '../../image/card/money.png';
 import CardLinkIcon from '../../image/card/open_card.png';
 
 export default function Card({ item }) {
+  const user = useSelector((state) => state.user);
   return (
     <li className="card">
       <div className="card_items_icon">
@@ -22,6 +23,9 @@ export default function Card({ item }) {
             src={NoMoneyIcon}
             alt="карточка за спасибо"
           />
+        )}
+        {item.invite?.length > 0 && user.uid === item.author && (
+          <span className="card_invite_value" />
         )}
         <Link className="card_link" to={`/card/${item.uid}`}>
           <img
